973d581e89dd5d9ef5e95f8b30c6e13a2d690f0d
Tien Dat <datsam20@gmail.com>
2022-07-12 21:11:13 +0700

    LAB02_2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p> LAB02.1</p>
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Lab React</NavbarBrand>
+            </div>
+          </Navbar>
+        </div>
       </div>
     );
   }
