fab56a07ee75b9bfd876ddcadbc334aca593acba
Tien Dat <datsam20@gmail.com>
2022-07-17 23:12:00 +0700

    Lab04_1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
-import StaffList from "./components/StaffListComponent";
-import { STAFFS } from "./shared/staffs";
+import Main from "./components/MainComponent";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { staffs: STAFFS };
-  }
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        <StaffList staffs={this.state.staffs} />
+        <Main />
       </div>
     );
   }
