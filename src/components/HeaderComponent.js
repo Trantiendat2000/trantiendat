f19438ad5e9e1a9cc04ff4b534cc0290836f9bb6
Tien Dat <datsam20@gmail.com>
2022-07-19 01:08:38 +0700

    Header Footer

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+
+class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar dark>
+          <div className="container">
+            <NavbarBrand href="/">Lab05</NavbarBrand>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Lab 05</h1>
+                <p>
+                  we take inspiration from the World's best cuisines, and create
+                  a unique fusion experienc. Ourlipsmacking creations will
+                  tickle you culinary senses!
+                </p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
